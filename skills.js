16b2eb15d89452321844788ecf3a27188048c657
Amir Bakhtiary <amir.bakhtiary@cluepoints.com>
2024-12-22 21:16:06 +0100

    Add skilsMember function to handle different input cases

--- skills.js
+++ skills.js
@@ -1,3 +1,12 @@
 function calculateNumbers(var1, var2) {
   return var1 + var2;
 }
+
+function skilsMember(var1){
+  if(var1 == "test") 
+    print("this is test")
+  else if(var1== "real")
+    print("this is real")
+  else
+    print("Nothing!")
+}
